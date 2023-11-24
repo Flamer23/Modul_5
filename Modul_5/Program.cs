ddcb36daf26281165eccbd8ab702d455288764be
Илья Зуев <zueviliya777@gmail.com>
2023-11-24 11:40:12 +0300

    Задание 5.1.5

--- Modul_5/Program.cs
+++ Modul_5/Program.cs
@@ -16,6 +16,58 @@
                 User.Dishes[i] = Console.ReadLine();
             }
 
+
+            //Задание 5.1.5
+            string[] favcolors = new string[3];
+            Console.WriteLine("Введи три своих любимых цвета");
+            for (int i = 0;i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            Console.WriteLine("Твои любимые цвета: ");
+            int j = 0;
+            foreach(var color in favcolors)
+            {
+                j++;
+                switch(color)
+                {
+                    case "red":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{j}) {color}");
+                            break;
+
+                        }
+                    case "green":
+                        {
+                            Console.ForegroundColor= ConsoleColor.Green;
+                            Console.WriteLine($"{j}) {color}");
+                            break;
+                        }
+                    case "yellow":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{j}) {color}");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine($"{j}) Неизвестный цвет"); break;
+                        }
+
+
+                }
+            }
+
+        }
+        static string ShowColor()
+        {
+            string color = Console.ReadLine();
+            return color;
+            //я до конца не понял, нужно ли было создать свой метод или использовать метод из курса
+            //решил сделать свой
         }
     }
 }
