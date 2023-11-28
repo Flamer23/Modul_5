72fe9e002d7dbbf5ea7d37130aee1473929f45d4
Илья Зуев <zueviliya777@gmail.com>
2023-11-28 16:34:20 +0300

    Задание 5.3.13

--- Modul_5/Program.cs
+++ Modul_5/Program.cs
@@ -4,182 +4,178 @@
     {
         static void Main(string[] args)
         {
-            ////Задание 1
-            //(string Name, string[] Dishes) User;
-            //Console.WriteLine("Как вас зовут?");
-            //User.Name = Console.ReadLine();
+            //Задание 1
+            (string Name, string[] Dishes) User;
+            Console.WriteLine("Как вас зовут?");
+            User.Name = Console.ReadLine();
 
-            //User.Dishes = new string[5];
-            //Console.WriteLine("Назови 5 своих любимых блюда");
-            //for(int i = 0; i < User.Dishes.Length; i++)
-            //{
-            //    User.Dishes[i] = Console.ReadLine();
-            //}
-
-
-            ////Задание 5.1.5
-            //string[] favcolors = new string[3];
-            //for (int i = 0;i < favcolors.Length; i++)
-            //{
-            //    favcolors[i] = ShowColor();
-            //}
-            //Console.ForegroundColor = ConsoleColor.White;
-
-            ////Задание 5.1.6
-            //int [] result = new int[5];
-            //result = GetArrayFromConsole();
-            //foreach (var element in result)
-            //{
-            //    Console.Write($"{element} ");
-            //}
+            User.Dishes = new string[5];
+            Console.WriteLine("Назови 5 своих любимых блюда");
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                User.Dishes[i] = Console.ReadLine();
+            }
 
 
-            ////Задание 5.2.2
-            //(string name, int age) anketa;
+            //Задание 5.1.5
+            string[] favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
 
-            //Console.Write("Введите имя: ");
-            //anketa.name = Console.ReadLine();
-            //Console.Write("Введите возраст с цифрами: ");
-            //anketa.age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Ваше имя: {0}", anketa.name);
-            //Console.WriteLine("Ваш возраст: {0}", anketa.age);
-            //for (int i = 0; i < favcolors.Length; i++)
-            //{
-            //    favcolors[i] = ShowColor2(anketa.name);
-            //}
+            //Задание 5.1.6
+            int[] result = new int[5];
+            result = GetArrayFromConsole();
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
 
-            ////Задание 5.2.3
-            //(string name, int age) = ("Илья", 21);
-            //Console.Write("Введите имя: ");
-            //anketa.name = Console.ReadLine();
-            //Console.Write("Введите возраст с цифрами: ");
-            //anketa.age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Ваше имя: {0}", name);
-            //Console.WriteLine("Ваш возраст: {0}", age);
-            //for (int i = 0; i < favcolors.Length; i++)
-            //{
-            //    favcolors[i] = ShowColor2(name);
-            //}
 
-            ////Задание 5.2.7
-            //Console.Write("Введите имя: ");
-            //anketa.name = Console.ReadLine();
-            //Console.Write("Введите возраст с цифрами: ");
-            //anketa.age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Ваше имя: {0}", name);
-            //Console.WriteLine("Ваш возраст: {0}", age);
-            //for (int i = 0; i < favcolors.Length; i++)
-            //{
-            //    favcolors[i] = ShowColor3(name, age);
-            //}
+            //Задание 5.2.2
+            (string name, int age) anketa;
 
-            ////Задание 5.2.8
-            //result = GetArrayFromConsole();
-            //result = SortArray(result);
-            //foreach (var element in result)
-            //{
-            //    Console.Write($"{element} ");
-            //}
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor2(anketa.name);
+            }
 
-            ////Задание 5.2.14
-            //int num = 5;
-            //result = GetArrayFromConsole2(ref num);
-            //result = SortArray(result);
-            //foreach (var element in result)
-            //{
-            //    Console.Write($"{element} ");
-            //}
+            //Задание 5.2.3
+            (string name, int age) = ("Илья", 21);
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor2(name);
+            }
 
-            ////Задание 5.2.15
-            //result = GetArrayFromConsole2(ref num);
-            //result = SortArray(result);
-            //foreach (var element in result)
-            //{
-            //    Console.Write($"{element} ");
-            //}
+            //Задание 5.2.7
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor3(name, age);
+            }
 
-            ////Задание 5.2.17
-            //result = GetArrayFromConsole();
-            //ShowArray(result);
+            //Задание 5.2.8
+            result = GetArrayFromConsole();
+            result = SortArray(result);
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
 
-            ////Задание 5.2.18
-            //result = GetArrayFromConsole2(ref num);
-            //ShowArray(result);
+            //Задание 5.2.14
+            int num = 5;
+            result = GetArrayFromConsole2(ref num);
+            result = SortArray(result);
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
 
-            ////Задание 5.3.1
-            //var someAge = 21;
-            //Console.WriteLine(someAge.ToString());
-            //ChangeAge(ref someAge);
-            //Console.WriteLine(someAge);
+            //Задание 5.2.15
+            result = GetArrayFromConsole2(ref num);
+            result = SortArray(result);
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
 
-            ////Задание 5.3.8
-            //num = 6;
-            //result = GetArrayFromConsole2(ref num);
-            //result = SortArray(result);
-            //foreach (var element in result)
-            //{
-            //    Console.Write($"{element} ");
-            //}
+            //Задание 5.2.17
+            result = GetArrayFromConsole();
+            ShowArray(result);
+
+            //Задание 5.2.18
+            result = GetArrayFromConsole2(ref num);
+            ShowArray(result);
+
+            //Задание 5.3.1
+            var someAge = 21;
+            Console.WriteLine(someAge.ToString());
+            ChangeAge(ref someAge);
+            Console.WriteLine(someAge);
+
+            //Задание 5.3.8
+            num = 6;
+            result = GetArrayFromConsole2(ref num);
+            result = SortArray(result);
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
 
             //Задание 5.3.13
-            Console.WriteLine("\nЗадание 5.3.13");
-            Random random = new Random();
-            int[] array = new int[10];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(-100,100);
-            }
-            int[] sortedAsc = new int[10];
-            int[] sortedDesc = new int[10];
-            SortArray2(array, out sortedAsc,out sortedDesc);
-            foreach (var element in sortedAsc)
-            {
-                Console.Write($"{element}, ");
-            }
-            Console.WriteLine("\n");
-            foreach (var element in sortedDesc)
-            {
-                Console.Write($"{element}, ");
-            }
+            int[] inputArray = { 5, 2, 8, 1, 7 };
+            int[] sortedDesc;
+            int[] sortedAsc;
+
+            SortArray2(inputArray, out sortedDesc, out sortedAsc);
+
+            Console.WriteLine("Sorted Descending: " + string.Join(", ", sortedDesc));
+            Console.WriteLine("Sorted Ascending: " + string.Join(", ", sortedAsc));
         }
         static void SortArray2(in int[] array, out int[] sortedDesc, out int[] sortedAsc)
         {
             sortedAsc = SortArrayAsc(array);
             sortedDesc = SortArrayDesc(array);
+
         }
-        static int[] SortArrayDesc(int[] result)
+        static int[] SortArrayDesc(int[] array)
         {
+            int[] sorted = new int[array.Length];
+            Array.Copy(array, 0, sorted, 0, sorted.Length);
             int temp;
-            for (var i = 1; i < result.Length; i++)
+            for (var i = 1; i < sorted.Length; i++)
             {
-                for (var j = 0; j < result.Length - i; j++)
+                for (var j = 0; j < sorted.Length - i; j++)
                 {
-                    if (result[j] < result[j + 1])
+                    if (sorted[j] > sorted[j + 1])
                     {
-                        temp = result[j];
-                        result[j] = result[j + 1];
-                        result[j + 1] = temp;
+                        temp = sorted[j];
+                        sorted[j] = sorted[j + 1];
+                        sorted[j + 1] = temp;
                     }
                 }
             }
-            return result;
+            return sorted;
         }
-        static int[] SortArrayAsc(int[] result)
+
+        static int[] SortArrayAsc(int[] array)
         {
+
+            int[] sorted = new int[array.Length];
+            Array.Copy(array, 0, sorted, 0, sorted.Length);
             int temp;
-            for (var i = 1; i < result.Length; i++)
+            for (var i = 1; i < sorted.Length; i++)
             {
-                for (var j = 0; j < result.Length - i; j++)
+                for (var j = 0; j < sorted.Length - i; j++)
                 {
-                    if (result[j] > result[j + 1])
+                    if (sorted[j] < sorted[j + 1])
                     {
-                        temp = result[j];
-                        result[j] = result[j + 1];
-                        result[j + 1] = temp;
+                        temp = sorted[j];
+                        sorted[j] = sorted[j + 1];
+                        sorted[j + 1] = temp;
                     }
                 }
             }
-            return result;
+            return sorted;
         }
         static void ChangeAge(ref int age)
         {
